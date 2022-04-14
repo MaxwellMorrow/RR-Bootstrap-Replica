54ed072565e17e6229381bc8fcda69b79aa4155b
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-04-14 14:39:05 -0700

    implimented nav bar under TopBar.js

---
+++ src/TopBar.js
@@ -0,0 +1,21 @@
+import  Navbar  from "react-bootstrap/Navbar";
+import  Nav  from "react-bootstrap/Nav";
+import  Button  from "react-bootstrap/Button";
+
+export default function TopBar(){
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
+          <img src="../public/Insta-Cart-Logo.png" alt="insta-cart-logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+}
